e4657731445b90d863e36199460247d7de158b90
mquarato <mq451@stern.nyu.edu>
2017-07-28 14:10:01 -0400

    Started stock performance functionality

--- freestyle.py
+++ freestyle.py
@@ -8,40 +8,67 @@ import csv
 csv_file_path = "data/stock_prices.csv"
 
 print("\nSTOCK PRICE LOOKUP APPLICATION\n")
-print("Today's date is: " +  str(datetime.date.today()))
+print("Welcome! Today's date is: " +  str(datetime.date.today()))
+print("\nPlease select one of the following options by inputting the corresponding number: ")
+print("\n1 - STOCK PRICE")
+print("2 - STOCK PERFORMANCE")
+
+operation = input("")
 
 stock_symbols = []
 
-while True:
-    symbol = input("\nPlease select a stock by symbol or 'DONE' if there are no more items: ")
-    if symbol == "DONE":
-        break
-    else:
-        stock_symbols.append(symbol)
 
-date_start = input("Please select a start date in the format yyyy-mm-dd: ")
-end_date = input("Please select an end date in the format yyyy-mm-dd: ")
+def price_lookup():
+    while True:
+        symbol = input("\nPlease select a stock by symbol or 'DONE' if there are no more items: ")
+        if symbol == "DONE":
+            break
+        else:
+            stock_symbols.append(symbol)
 
-date_start = datetime.datetime.strptime(date_start, '%Y-%m-%d')
-end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+    date_start = input("Please select a start date in the format yyyy-mm-dd: ")
+    end_date = input("Please select an end date in the format yyyy-mm-dd: ")
+
+    date_start = datetime.datetime.strptime(date_start, '%Y-%m-%d')
+    end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
 
 # COMPILE REQUEST PARAMS
-
-data_source = 'google'
-
+    data_source = 'google'
 # ISSUE REQUEST
-
-response = data.DataReader(stock_symbols, data_source, date_start, end_date)
-
+    response = data.DataReader(stock_symbols, data_source, date_start, end_date)
 # PARSE RESPONSE
+    daily_closing_prices = response.ix["Close"] # ix() is a pandas DataFrame function
 
-daily_closing_prices = response.ix["Close"] # ix() is a pandas DataFrame function
-print(type(daily_closing_prices))
-print("\nHere are the Stock Prices for the days you indicated:\n")
-print(daily_closing_prices)
-confirmation = input("\nWould you like to save this data to a file? (Y/N)")
-if confirmation == "Y":
+    print("\nHere are the Stock Prices for the days you indicated:\n")
+    print(daily_closing_prices)
+    confirmation = input("\nWould you like to save this data to a file? (Y/N) ")
+    if confirmation == "Y":
+        prices = daily_closing_prices.to_csv(csv_file_path)
+        print("Great! The data has been saved")
+    else:
+        print("OK. The data is not saved")
+
+
+def performance():
+    symbol =input("Please select a stock by symbol: ")
+    stock_symbols.append(symbol)
+
+    date_start = input("Please select a start date in the format yyyy-mm-dd: ")
+    end_date = input("Please select an end date in the format yyyy-mm-dd: ")
+
+    date_start = datetime.datetime.strptime(date_start, '%Y-%m-%d')
+    end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+
+    number_of_stocks = input("Please input number of stocks owned: ")
+
+    data_source = 'google'
+
+    response = data.DataReader(stock_symbols, data_source, date_start, end_date)
+    daily_closing_prices = response.ix["Close"]
     prices = daily_closing_prices.to_csv(csv_file_path)
-    print("Great! The data has been saved")
+
+    print("Performance")
+if operation == "1": price_lookup()
+elif operation == "2": performance()
 else:
-    print("OK. We won't save the data")
+    print("PLEASE CHOOSE ONE OF THE AVAILABLE OPERATIONS")
